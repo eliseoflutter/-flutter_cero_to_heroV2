0f831b6a0ed0bdeb9ad2f3002e4a38ddddd3b190
Oscar Chavez <oscarchb04@gmail.com>
2020-09-23 03:46:30 -0400

    *Agrege los codigos de los ultimos dos videos

--- proyecto_ejemplo/lib/widgets/images.dart
+++ proyecto_ejemplo/lib/widgets/images.dart
@@ -1,40 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter 0 a 100',
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
+class ProImagesWidget extends StatelessWidget {
   final imageUrl =
       'https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg-1024x683.jpg';
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: Colors.red,
-      body: SafeArea(
-        child: Center(
-          child: FadeInImage(
-            image: NetworkImage(imageUrl),
-            placeholder: AssetImage('assets/gif/loading_clear.gif'),
-            // placeholder: AssetImage('assets/gif/loading_fill.gif'),
-            // placeholder: AssetImage('assets/gif/loading_normal.gif'),
-            height: 400,
-            width: 400,
-            fit: BoxFit.contain,
-            // extras
-            fadeInDuration: Duration(milliseconds: 500),
-          ),
-        ),
-      ),
+    return Image.network(
+      imageUrl,
+      height: 400,
+      width: 400,
+      fit: BoxFit.contain,
     );
   }
 }
